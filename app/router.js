00d41fbcaf9c0b9ee14e478620c574aa0a38a611
Ken Schmidt <kenschmidt@gmail.com>
2014-02-16 12:39:39 -0800

    Add a person template

--- app/router.js
+++ app/router.js
@@ -3,7 +3,9 @@ var Router = Ember.Router.extend(); // ensure we don't share routes between all 
 Router.map(function() {
   this.route('component-test');
   this.route('helper-test');
-  this.resource('people');
+  this.resource('people', function () {
+	this.resource('person', {path: ':person_id'});
+  });
   // this.resource('posts', function() {
   //   this.route('new');
   // });
